9552c14363283c2a7d8dd68510549aee05bf4f27
amiableColin <142316711+amiableColin@users.noreply.github.com>
2024-06-23 20:14:16 +0900

    Create atm_11399.java

---
+++ GREEDY/atm_11399.java
@@ -0,0 +1,19 @@
+package GREEDY;
+import java.util.Arrays;
+import java.util.Scanner;
+public class atm_11399 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        int[] P = new int[N];
+        for(int i = 0; i < N; i++) {
+            P[i] = s.nextInt();
+        }
+        Arrays.sort(P);
+        for(int i = 1; i < N; i++) {
+            P[i] += P[i-1];
+        }
+        System.out.println(Arrays.stream(P).sum());
+        s.close();
+    }
+}
