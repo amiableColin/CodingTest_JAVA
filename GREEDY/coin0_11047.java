0f4b4e907eba7d95409b5ac6e74d4edb528466ff
amiableColin <142316711+amiableColin@users.noreply.github.com>
2024-06-23 20:21:47 +0900

    Create coin0_11047.java

---
+++ GREEDY/coin0_11047.java
@@ -0,0 +1,20 @@
+package GREEDY;
+import java.util.Scanner;
+public class coin0_11047 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        int K = s.nextInt();
+        int[] coins = new int[N];
+        int coin_cnt = 0;
+        for (int i = 0; i < coins.length; i++) {
+            coins[i] = s.nextInt();
+        }
+        for(int i = coins.length - 1; i >= 0; i--) {
+            coin_cnt += (K / coins[i]);
+            K %= coins[i];
+        }
+        System.out.println(coin_cnt);
+        s.close();
+    }
+}
